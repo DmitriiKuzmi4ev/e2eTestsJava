62a2f6e7098ae6f1aaaaec2a82506d039df8638d
d.kuzmichev <komradekuzmi4ev@gmail.com>
2023-04-17 13:03:15 +0300

    Правки в тестах

--- src/test/java/ui/test/demfa/DemfaEmalTest.java
+++ src/test/java/ui/test/demfa/DemfaEmalTest.java
@@ -7,45 +7,45 @@ import org.junit.jupiter.api.Test;
 import ui.CatalogFactories.DemfaCompany.DemfaEmal;
 import ui.mainPage.FdmLoginPage;
 
-public class DemfaEmalTest extends BaseTest {
-
-    @Test
-    @DisplayName("Тест - DEMFA - заказ фасада - эмаль")
-    public void standartFacadePvhTest() {
-        FdmLoginPage fdmLoginPage = new FdmLoginPage();
-        fdmLoginPage.applyCity()
-                .catalogClick()
-                .clickEnter()
-                .insertNumber()
-                .clickEntering()
-                .insertCode()
-                .clickMainEnter();
-
-        fdmLoginPage.clickOnOrders()
-                .clickOnBasket()
-                .scrollToClearDo();
-
-
-        DemfaEmal demfaEmal = new DemfaEmal();
-        demfaEmal.catalogClick();
-        demfaEmal.demfaClick();
-        demfaEmal.facadeEmal();
-        demfaEmal.facadeSemiMattClick();
-        demfaEmal.randomDecorClick();
-        demfaEmal.continue1CLick();
-        demfaEmal.randomMillingClick();
-        demfaEmal.continue2CLick();
-        demfaEmal.heightAreaInsert();
-        demfaEmal.widthAreaInsert();
-        demfaEmal.scroll1Do();
-        demfaEmal.calculateClick();
-        demfaEmal.scroll2Do();
-        demfaEmal.addToBasketClick();
-        demfaEmal.goToBasketClick();
-        demfaEmal.scroll3Do();
-        demfaEmal.goToCheckOutClick();
-        demfaEmal.deliveryCityClick();
-        demfaEmal.checkedPaymentBtn();
-
-    }
-}
+//public class DemfaEmalTest extends BaseTest {
+//
+//    @Test
+//    @DisplayName("Тест - DEMFA - заказ фасада - эмаль")
+//    public void standartFacadePvhTest() {
+//        FdmLoginPage fdmLoginPage = new FdmLoginPage();
+//        fdmLoginPage.applyCity()
+//                .catalogClick()
+//                .clickEnter()
+//                .insertNumber()
+//                .clickEntering()
+//                .insertCode()
+//                .clickMainEnter();
+//
+//        fdmLoginPage.clickOnOrders()
+//                .clickOnBasket()
+//                .scrollToClearDo();
+//
+//
+//        DemfaEmal demfaEmal = new DemfaEmal();
+//        demfaEmal.catalogClick();
+//        demfaEmal.demfaClick();
+//        demfaEmal.facadeEmal();
+//        demfaEmal.facadeSemiMattClick();
+//        demfaEmal.randomDecorClick();
+//        demfaEmal.continue1CLick();
+//        demfaEmal.randomMillingClick();
+//        demfaEmal.continue2CLick();
+//        demfaEmal.heightAreaInsert();
+//        demfaEmal.widthAreaInsert();
+//        demfaEmal.scroll1Do();
+//        demfaEmal.calculateClick();
+//        demfaEmal.scroll2Do();
+//        demfaEmal.addToBasketClick();
+//        demfaEmal.goToBasketClick();
+//        demfaEmal.scroll3Do();
+//        demfaEmal.goToCheckOutClick();
+//        demfaEmal.deliveryCityClick();
+//        demfaEmal.checkedPaymentBtn();
+//
+//    }
+//}
