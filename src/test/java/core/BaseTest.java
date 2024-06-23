a64ca1d5f7a8484678c59e0e1d9844cf2fc6a91e
user <komradekuzmi4ev@gmail.com>
2024-06-23 13:07:25 +0300

    - add api test for ci/cd example/1

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -9,7 +9,8 @@ public class BaseTest {
     private static final String BASE_URL = "https://dev.allfdm.ru/";
 
     private void init() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Red\\chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Red\\chromedriver.exe");
+        Configuration.driverManagerEnabled = true;
         Configuration.browserSize = "1920x1080";
         Configuration.headless = true;
         Configuration.holdBrowserOpen = false;
@@ -18,7 +19,7 @@ public class BaseTest {
         Configuration.fastSetValue = true;
         Configuration.screenshots = false;
         Selenide.clearBrowserCookies();
-        Selenide.open(BASE_URL);
+        //Selenide.open(BASE_URL);
         Selenide.clearBrowserLocalStorage();
     }
 
